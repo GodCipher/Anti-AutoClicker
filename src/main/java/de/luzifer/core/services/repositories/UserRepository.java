4ff3411313aa54de8f8beae3dc0305acfe9c834f
Nadure <92591280+nadvre@users.noreply.github.com>
2022-01-31 20:38:20 +0100

    added UserRepository

---
+++ src/main/java/de/luzifer/core/services/repositories/UserRepository.java
@@ -0,0 +1,45 @@
+package de.luzifer.core.services.repositories;
+
+import de.luzifer.core.model.player.User;
+import de.luzifer.core.model.exceptions.UserNotFoundException;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+public class UserRepository {
+
+    private final Set<User> userSet = new HashSet<>();
+
+    public User create(User user) {
+
+         userSet.add(user);
+
+         return user;
+    }
+
+    public User read(UUID id) throws UserNotFoundException {
+
+        for (User user : userSet)
+            if (user.getPlayer().getUniqueId().equals(id))
+                return user;
+
+        throw new UserNotFoundException("User could not be found.");
+    }
+
+
+    @Deprecated
+    public User update(UUID id, User user) {
+        // not used
+        throw new UnsupportedOperationException("This methods isn't allowed.");
+    }
+
+    public void delete(UUID id) throws UserNotFoundException {
+
+        for (User user : userSet)
+            if (user.getPlayer().getUniqueId().equals(id))
+                userSet.remove(user);
+
+        throw new UserNotFoundException("User could not be found.");
+    }
+}
