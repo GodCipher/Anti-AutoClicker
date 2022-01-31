ee39200e624c8de7dad5138d222b43315d95cd2b
Luziferium <luziferisdoof@gmail.com>
2022-01-31 22:00:34 +0100

    Added annotation and created Userdata template

--- src/main/java/de/luzifer/core/model/user/cps/Cps.java
+++ src/main/java/de/luzifer/core/model/user/cps/Cps.java
@@ -1,8 +1,10 @@
 package de.luzifer.core.model.user.cps;
 
+import lombok.EqualsAndHashCode;
 import lombok.Value;
 
 @Value(staticConstructor = "of")
+@EqualsAndHashCode
 public class Cps {
     int value;
 }
--- src/main/java/de/luzifer/core/model/user/violation/ViolationLevel.java
+++ src/main/java/de/luzifer/core/model/user/violation/ViolationLevel.java
@@ -1,8 +1,10 @@
 package de.luzifer.core.model.user.violation;
 
+import lombok.EqualsAndHashCode;
 import lombok.Value;
 
 @Value(staticConstructor = "of")
+@EqualsAndHashCode
 public class ViolationLevel {
     int level;
 }
