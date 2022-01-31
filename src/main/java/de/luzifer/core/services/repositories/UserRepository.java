21b65699d8784bb22e1fb5cd4e72a8c6647eff62
Nadure <92591280+nadvre@users.noreply.github.com>
2022-01-31 20:49:04 +0100

    fixed delete and added javadoc for update in UserRepository

--- src/main/java/de/luzifer/core/services/repositories/UserRepository.java
+++ src/main/java/de/luzifer/core/services/repositories/UserRepository.java
@@ -12,9 +12,7 @@ public class UserRepository {
     private final Set<User> userSet = new HashSet<>();
 
     public User create(User user) {
-
          userSet.add(user);
-
          return user;
     }
 
@@ -27,19 +25,22 @@ public class UserRepository {
         throw new UserNotFoundException("User could not be found.");
     }
 
-
+    /**
+     * @deprecated since 3.0
+     */
     @Deprecated
     public User update(UUID id, User user) {
-        // not used
-        throw new UnsupportedOperationException("This methods isn't allowed.");
+        throw new UnsupportedOperationException("This method is not yet implemented.");
     }
 
     public void delete(UUID id) throws UserNotFoundException {
 
-        for (User user : userSet)
-            if (user.getPlayer().getUniqueId().equals(id))
+        for (User user : userSet) {
+            if (user.getPlayer().getUniqueId().equals(id)) {
                 userSet.remove(user);
-
+                return;
+            }
+        }
         throw new UserNotFoundException("User could not be found.");
     }
 }
