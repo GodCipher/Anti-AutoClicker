7ca946852c8164ad77a3c11eec122f9f043d8948
Nadure <92591280+nadvre@users.noreply.github.com>
2022-01-31 21:03:40 +0100

    Added ViolationType

---
+++ src/main/java/de/luzifer/core/model/user/violation/ViolationType.java
@@ -0,0 +1,21 @@
+package de.luzifer.core.model.user.violation;
+
+import de.luzifer.core.Core;
+
+public enum ViolationType {
+
+    EASY(Core.getInstance().getConfig().getInt("AntiAC.Easy-Violation")),
+    NORMAL(Core.getInstance().getConfig().getInt("AntiAC.Normal-Violation")),
+    HARD(Core.getInstance().getConfig().getInt("AntiAC.Hard-Violation"));
+
+    private final int i;
+
+    ViolationType(int i) {
+        this.i = i;
+    }
+
+    public int getViolations() {
+        return i;
+    }
+
+}
