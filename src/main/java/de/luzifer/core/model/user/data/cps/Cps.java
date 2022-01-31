151c9c00a8b8f0b60dab5befb6da62123207681f
Nadure <92591280+nadvre@users.noreply.github.com>
2022-01-31 22:30:56 +0100

    Removed @HashcodeAndEquals Annotation

--- src/main/java/de/luzifer/core/model/user/data/cps/Cps.java
+++ src/main/java/de/luzifer/core/model/user/data/cps/Cps.java
@@ -1,10 +1,8 @@
 package de.luzifer.core.model.user.data.cps;
 
-import lombok.EqualsAndHashCode;
 import lombok.Value;
 
 @Value(staticConstructor = "of")
-@EqualsAndHashCode
 public class Cps {
     int value;
 }
