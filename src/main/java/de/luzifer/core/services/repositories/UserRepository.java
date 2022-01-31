8b6f2ac4571add45b88d25d05ad2697d219f33cd
Nadure <92591280+nadvre@users.noreply.github.com>
2022-01-31 22:03:46 +0100

    Changed UserRepository to an interface

--- src/main/java/de/luzifer/core/services/repositories/UserRepository.java
+++ src/main/java/de/luzifer/core/services/repositories/UserRepository.java
@@ -1,46 +1,20 @@
 package de.luzifer.core.services.repositories;
 
 import de.luzifer.core.model.user.User;
-import de.luzifer.core.model.exceptions.UserNotFoundException;
 
-import java.util.HashSet;
-import java.util.Set;
 import java.util.UUID;
 
-public class UserRepository {
+public interface UserRepository {
 
-    private final Set<User> userSet = new HashSet<>();
+    User create(User user);
 
-    public User create(User user) {
-         userSet.add(user);
-         return user;
-    }
-
-    public User read(UUID id) throws UserNotFoundException {
-
-        for (User user : userSet)
-            if (user.getPlayer().getUniqueId().equals(id))
-                return user;
-
-        throw new UserNotFoundException("User could not be found.");
-    }
+    User read(UUID uuid);
 
     /**
      * @deprecated since 3.0
      */
     @Deprecated
-    public User update(UUID id, User user) {
-        throw new UnsupportedOperationException("This method is not yet implemented.");
-    }
+    User update(UUID uuid, User user);
 
-    public void delete(UUID id) throws UserNotFoundException {
-
-        for (User user : userSet) {
-            if (user.getPlayer().getUniqueId().equals(id)) {
-                userSet.remove(user);
-                return;
-            }
-        }
-        throw new UserNotFoundException("User could not be found.");
-    }
+    void delete(UUID uuid);
 }
---
+++ src/main/java/de/luzifer/core/services/repositories/implementation/UserRepositoryImpl.java
@@ -0,0 +1,47 @@
+package de.luzifer.core.services.repositories.implementation;
+
+import de.luzifer.core.model.user.User;
+import de.luzifer.core.model.exceptions.UserNotFoundException;
+import de.luzifer.core.services.repositories.UserRepository;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+public class UserRepositoryImpl implements UserRepository {
+
+    private final Set<User> userSet = new HashSet<>();
+
+    @Override
+    public User create(User user) {
+         userSet.add(user);
+         return user;
+    }
+
+    @Override
+    public User read(UUID id) throws UserNotFoundException {
+
+        for (User user : userSet)
+            if (user.getPlayer().getUniqueId().equals(id))
+                return user;
+
+        throw new UserNotFoundException("User could not be found.");
+    }
+
+    @Override
+    public User update(UUID uuid, User user) {
+        throw new UnsupportedOperationException("This method is not yet implemented.");
+    }
+
+    @Override
+    public void delete(UUID id) throws UserNotFoundException {
+
+        for (User user : userSet) {
+            if (user.getPlayer().getUniqueId().equals(id)) {
+                userSet.remove(user);
+                return;
+            }
+        }
+        throw new UserNotFoundException("User could not be found.");
+    }
+}
