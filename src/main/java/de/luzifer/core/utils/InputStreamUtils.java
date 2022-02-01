61b6b5bae3811e565be215ed1e63a2b76002c62a
Luziferium <luziferisdoof@gmail.com>
2022-02-01 10:52:11 +0100

    Added JavaDocs

--- src/main/java/de/luzifer/core/utils/InputStreamUtils.java
+++ src/main/java/de/luzifer/core/utils/InputStreamUtils.java
@@ -7,11 +7,15 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.logging.Logger;
 
 @UtilityClass
 public class InputStreamUtils {
-    
+
+    /**
+     * Returns the InputStream of a Resource with the given name
+     */
     public InputStream getInputStream(String fileName) {
         
         InputStream resource = Core.class.getResourceAsStream("/" + fileName);
@@ -21,10 +25,14 @@ public class InputStreamUtils {
         
         return resource;
     }
-    
+
+    /**
+     * Uses a {@link BufferedReader} to read the first line of text from the given {@link InputStream}.
+     * Autocloses the InputStream afterwards
+     */
     public String readLineFromInputStream(InputStream inputStream, Logger logger) {
         
-        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream))) {
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
             return bufferedReader.readLine();
         } catch (IOException e) {
             logger.warning("Something exploded, reload/restart and try again");
